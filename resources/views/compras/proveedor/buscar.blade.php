2057dc5973c19e3d0e0c4bc5bd891e280d8bd244
Thanatos-PC\Thanatos <sergioaleg_34@hotmail.com>
2017-01-16 16:54:53 -0500

    no message

--- resources/views/compras/proveedor/buscar.blade.php
+++
@@ -1,8 +0,0 @@
-{!! Form::open(['url'=>'compras/proveedor', 'method'=>'GET', 'autocomplete'=>'off', 'role'=>'search']) !!}
-<div class="form-group">
-	<div class="input-group">
-		<input type="text" class="form-control" name="FiltroBusqueda" placeholder="Buscar...." value="{{$FiltroBusqueda}}">
-		<span class="input-group-btn"><button type="submint" class="btn btn-primary">Buscar</button></span>
-	</div>
-</div>
-{!! Form::close() !!}
